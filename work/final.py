0d6698a0a85ba2f9cea86d3ad57044adf48bf813
ant-one-dev <antoinemroczko@gmail.com>
2026-07-03 19:17:22 +0200

    auto transition final.py (tout sauf labyrinthe vers IR)

--- work/final.py
+++ work/final.py
@@ -92,8 +92,6 @@ class RobotStepManager:
                                      name="US_IR", daemon=True),
                     threading.Thread(target=t11_thread_line, args=(robot_instance, args_instance.sensor_interval),
                                      name="LINE_IR", daemon=True),
-                    threading.Thread(target=t11_thread_LED, args=(robot_instance, args_instance.sensor_interval),
-                                     name="LED_IR", daemon=True),
                     threading.Thread(target=t11_thread_controller, args=(robot_instance, args_instance.ctrl_interval),
                                      name="CTRL_IR", daemon=True),
                     threading.Thread(target=t11_thread_buzzer, args=(robot_instance,), name="BUZZER", daemon=True),
@@ -124,8 +122,6 @@ class RobotStepManager:
                                      args=(robot_instance, camera_line3.CTRL_INTERVAL), name="CTRL", daemon=True),
                     threading.Thread(target=camera_line3.thread_ultrasonic,
                                      args=(robot_instance, camera_line3.US_INTERVAL), name="US", daemon=True),
-                    threading.Thread(target=camera_line3.thread_LED, args=(robot_instance, camera_line3.LED_INTERVAL),
-                                     name="LED", daemon=True),
                     threading.Thread(target=camera_line3.thread_camera_loop, args=(robot_instance, self.camera),
                                      name="CAM_AUTO", daemon=True),
                     threading.Thread(
