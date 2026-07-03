fe144d23bc3f478febcada6e6698a3b06c21ebfb
ant-one-dev <antoinemroczko@gmail.com>
2026-07-03 19:24:46 +0200

    auto transition final.py (tout sauf labyrinthe vers IR)

--- work/transitions.py
+++ work/transitions.py
@@ -13,7 +13,7 @@ def calibration_sequence_IR_to_obstacles(robot_instance: Robot) -> str:
     robot_instance.motor.reset()
     robot_instance.head.set_angle_motor(0, 90)
     robot_instance.motor.drive(camera_line3.Direction.FORWARD, 20, fast_accel=True)
-    time.sleep(0.4)
+    time.sleep(1.5)
     robot_instance.motor.reset()
 
 
@@ -30,7 +30,7 @@ def calibration_sequence_obstacles_to_camera_line(robot_instance: Robot) -> str:
 
     robot_instance.motor.reset()
     robot_instance.head.set_angle_motor(2, 60)
-    time.sleep(1.3)
+    time.sleep(0.4)
 
     print("[SYS] CALIBRATION_OK: LIGNE_ROUGE -> Passage automatique à 'Camera Line'")
     return "Camera Line"
