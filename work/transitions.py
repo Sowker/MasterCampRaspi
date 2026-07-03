2420c6c0655d99feb51e67c710563bbef7556f85
ant-one-dev <antoinemroczko@gmail.com>
2026-07-03 18:58:50 +0200

    auto transition final.py (tout sauf labyrinthe vers IR)

--- work/transitions.py
+++ work/transitions.py
@@ -6,8 +6,7 @@ import camera_line3
 
 def calibration_sequence_IR_to_obstacles(robot_instance: Robot) -> None:
     """Séquence de mouvements pour se préparer à l'évitement d'obstacles."""
-    global log, target_step
-    log.info("[SYS] INIT_CALIBRATION: OBSTACLES -> Réinitialisation odométrie & capteurs")
+    global target_step
 
     with robot_instance.state.lock:
         robot_instance.state.running = False
@@ -15,15 +14,12 @@ def calibration_sequence_IR_to_obstacles(robot_instance: Robot) -> None:
     robot_instance.motor.reset()
     robot_instance.head.set_angle_motor(0, 90)
     time.sleep(0.3)
-
-    log.info("[SYS] CALIBRATION_OK: OBSTACLES -> Transition demandée vers 'Obstacles'")
     target_step = "Obstacles"
 
 
 def calibration_sequence_obstacles_to_camera_line(robot_instance: Robot) -> None:
     """Séquence pour baisser la tête de la caméra vers le sol pour le suivi de ligne rouge."""
-    global log, target_step
-    log.info("[SYS] INIT_CALIBRATION: LIGNE_ROUGE -> Inclinaison caméra basse (60deg)")
+    global target_step
 
     with robot_instance.state.lock:
         robot_instance.state.running = False
@@ -32,7 +28,6 @@ def calibration_sequence_obstacles_to_camera_line(robot_instance: Robot) -> None
     robot_instance.head.set_angle_motor(2, 60)
     time.sleep(0.3)
 
-    log.info("[SYS] CALIBRATION_OK: LIGNE_ROUGE -> Transition demandée vers 'Camera Line'")
     target_step = "Camera Line"
     with robot_instance.state.lock:
         robot_instance.state.running = True
